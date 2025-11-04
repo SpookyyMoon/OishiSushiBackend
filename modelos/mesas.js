0e36b7f5d156b1814aad151b3577b45005fb7fc6
SpookyLuna <spookyluna666@gmail.com>
2025-11-04 12:48:05 +0100

    Colección mesas

---
+++ modelos/mesas.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const ItemSchema = new mongoose.Schema({
+  numeroMesa: { type: Number, required: true },
+  ocupadaMesa: { type: Boolean, required: true },
+  comandasMesa: { type: Array, required: false }
+});
+
+export default mongoose.model('Mesas', ItemSchema);
