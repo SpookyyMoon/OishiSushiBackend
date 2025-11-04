3e9530a9ecaffba6d1797e450f056aa802f3a277
SpookyyMoon <spookyluna666@gmail.com>
2025-11-04 19:16:31 +0100

    Rutas del API

---
+++ rutas/itemRutas.js
@@ -0,0 +1,10 @@
+// rutas/itemRutas.js
+import express from "express";
+import comandas from "../modelos/comandas.js";
+import mesas from "../modelos/mesas.js";
+import platos from "../modelos/platos.js";
+
+const router = express.Router();
+
+
+export default router;
