c2d29cb6302751c837e287a2daced46d064c5edb
SpookyLuna <spookyluna666@gmail.com>
2025-11-14 12:02:15 +0100

    Actualizado comandas

--- modelos/comandas.js
+++ modelos/comandas.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const ComandaEschema = new mongoose.Schema({
   numeroMesa: { type: Number, required: true },
   pedidoPlatos: { type: Array, required: true },
-  atendidaComanda: { type: Boolean, required: true, default: false },
-  pagadoComanda: { type: Boolean, required: true, default: false },
+  atendidaComanda: { type: Boolean, required: true, default: false }
 });
 
 export default mongoose.model("Comanda", ComandaEschema);
